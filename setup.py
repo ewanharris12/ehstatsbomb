0b27165b612e4dad2014ee69ecd3a5eb078bacfd
Ewan Harris <54112442+ewanharris12@users.noreply.github.com>
2020-12-13 20:57:02 +0000

    rename to ehsb, adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='ehstatsbomb',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first attempt at building a Python library. Extracts, parses and performs functions on statsbomb event data.',
+    author='Ewan Harris',
+    license='Data kindly provided by Statsbomb',
+)
