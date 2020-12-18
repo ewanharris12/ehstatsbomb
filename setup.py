200e368d289ff4fc93bd1935357204834762ee37
Ewan Harris <54112442+ewanharris12@users.noreply.github.com>
2020-12-18 15:54:34 +0000

    v0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='ehstatsbomb',
     packages=find_packages(),
-    version='0.1.5',
+    version='0.1.6',
     description='My first attempt at building a Python library. Extracts, parses and performs functions on statsbomb event data.',
     author='Ewan Harris',
     license='Data kindly provided by Statsbomb',
